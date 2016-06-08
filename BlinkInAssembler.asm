b2968cf33e3924f297fae49b6387b918190f2b9c
chisaton <anderluehe13dsy4430@gmail.com>
2016-06-08 22:27:35 +0900

    add in assembly language

---
+++ BlinkInAssembler.asm
@@ -0,0 +1,30 @@
+/*
+ * BlinkInAssembler.asm
+ * Author: Chisato Nakamura
+ */
+
+.nolist
+#include "m328pdef.inc"
+.list
+
+.def DELAY_OUTER_LOOP = r22
+
+.def TEMP_REG = r20
+.def DATA_REG = r21
+.cseg
+
+ldi TEMP_REG, 0xFF
+out DDRB, TEMP_REG
+ldi DATA_REG, 0xFF
+
+LOOP:   out PORTB, DATA_REG
+DELAY:  ldi DELAY_OUTER_LOOP 200
+OUTER:  ldi YL, 0x3F ;changed here
+		ldi YH, 0x9C ;changed here
+INNER:  sbiw Y, 1
+		brne INNER
+		dec DELAY_PUTER_LOOP
+		brne OUTER
+
+		com DATA_REG
+		rjmp LOOP
